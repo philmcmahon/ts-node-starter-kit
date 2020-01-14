e19bcc7e75f5958bc0e5e3def07f1ffbf1570dc6
Nicolas Long <nicolas.long@theguardian.com>
2020-01-14 15:47:50 +0000

    Add initial config and packages

---
+++ README.md
@@ -0,0 +1,21 @@
+# Guardian Typescript server-side starter kit
+
+## We need
+
+packages:
+
+ts
+react
+express
+emotion
+aws-serverless-express
+jest
+ts-node-dev
+
+(and @types)
+
+config:
+
+tsconfig
+.editorconfig
+.vscode/extensions.json
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import awsServerlessExpress from "aws-serverless-express";
+import { Context } from "aws-lambda";
+
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/healthcheck", (req, res) =>
+    res.send({ msg: "hello Andre, Tom and Nic!" })
+);
+
+// If local then don't wrap in serverless
+if (process.env.NODE_ENV === "development") {
+    app.listen(3030);
+} else {
+    const server = awsServerlessExpress.createServer(app);
+    exports.handler = (event: any, context: Context) => {
+        awsServerlessExpress.proxy(server, event, context);
+    };
+}
