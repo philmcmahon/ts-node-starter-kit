7675ab0c39087dda13e6a3f3b0098fc169e60af6
Tom Wey <tom.wey@theguardian.com>
2020-01-14 17:06:19 +0000

    Log the port the Express server is listening on

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import awsServerlessExpress from 'aws-serverless-express';
 import { Context } from 'aws-lambda';
 
+const PORT = 3030;
+
 const app = express();
 
 app.use(express.json({ limit: '50mb' }));
@@ -10,7 +12,7 @@ app.get('/healthcheck', (req, res) => res.send({ msg: 'hello Andre, Tom and Nic!
 
 // If local then don't wrap in serverless
 if (process.env.NODE_ENV === 'development') {
-    app.listen(3030);
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 } else {
     const server = awsServerlessExpress.createServer(app);
     exports.handler = (event: any, context: Context) => {
