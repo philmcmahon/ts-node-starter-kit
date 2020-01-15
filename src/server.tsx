8cf22571d9faac63be72f916d6630cb23d909592
Andre Silva <andre.silva.freelancer@guardian.co.uk>
2020-01-15 14:50:55 +0000

    Tweaks order for readability

--- src/server.tsx
+++ src/server.tsx
@@ -7,6 +7,9 @@ import { renderToStaticMarkup } from 'react-dom/server';
 import { extractCritical } from 'emotion-server';
 import { headline } from '@guardian/src-foundations/typography';
 
+const app = express();
+app.use(express.json({ limit: '50mb' }));
+
 const ExampleComponent: React.FC<{}> = () => (
     <h1
         className={css`
@@ -17,17 +20,13 @@ const ExampleComponent: React.FC<{}> = () => (
     </h1>
 );
 
-const PORT = 3030;
-
-const app = express();
-app.use(express.json({ limit: '50mb' }));
-
 app.get('/', (req, res) => {
     const { html, css } = extractCritical(renderToStaticMarkup(<ExampleComponent />));
     res.send({ html, css });
 });
 
 // If local then don't wrap in serverless
+const PORT = 3030;
 if (process.env.NODE_ENV === 'development') {
     app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 } else {
